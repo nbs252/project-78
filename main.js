8065e3c68b562d86c0b051085e3d22128d243267
nbs252 <87271185+nbs252@users.noreply.github.com>
2021-07-14 20:00:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var names=["Naman", "Shilpa", "Biren", "Dhwani"];
+
+var i= 0;
+var function_submit(){
+i++;
+var number_of_family_member_in_array= 4;
+if(i > number_of_family_member_in_array)
+}
+i=0;
+{
+var updateName= names[i]
+document.getElementById("family_member_image").innerHTML= updateName;
+}
